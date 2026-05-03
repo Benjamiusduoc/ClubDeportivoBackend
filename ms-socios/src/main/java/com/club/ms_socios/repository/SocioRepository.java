13cfaab8df8e76bfdd8da074efc00e7d0f06a8ab
Brahyans2 <brah.rojas@duocuc.cl>
2026-05-03 15:03:07 -0400

    Actualizo logica del service

--- ms-socios/src/main/java/com/club/ms_socios/repository/SocioRepository.java
+++ ms-socios/src/main/java/com/club/ms_socios/repository/SocioRepository.java
@@ -4,6 +4,7 @@ import com.club.ms_socios.model.entity.Socio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,8 @@ public interface SocioRepository extends JpaRepository<Socio, Long> {
     // Spring Data JPA crea la consulta SQL automáticamente por debajo
     Optional<Socio> findByRut(String rut);
     Optional<Socio> findByEmail(String email);
+
+    List<Socio> findByActivoTrue();
+
+    long countByActivoTrue();
 }
